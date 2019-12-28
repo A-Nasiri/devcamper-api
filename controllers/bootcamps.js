0bab43bb6f9dde47f5734fd8ce2442def6a21318
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2019-12-28 12:24:34 +0000

    Create bootcamp POST route

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@desc         Get all bootcamps
 //@route        GET /api/v1/bootcamps
 //@access       Public
@@ -17,8 +19,19 @@ exports.getBootcamp = (req, res, next) => {
 //@desc         Create new bootcamp
 //@route        POST /api/v1/bootcamps
 //@access       Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create all bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
 };
 
 //@desc         Update bootcamp
