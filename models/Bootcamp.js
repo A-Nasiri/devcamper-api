17522443b45fb9a0e94ca04e3bf75b04c13bb496
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-03-09 18:49:59 +0000

    Cascade delete courses when a bootcamp is deleted

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -129,6 +129,12 @@ BootcampSchema.pre('save', async function(next) {
   next();
 });
 
+//Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
 //Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
